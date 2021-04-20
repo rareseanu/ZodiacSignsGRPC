90b7769aec8bb65d43848caf03af62967a512294
Rares Moiseanu <moiseanurares@gmail.com>
2021-04-20 14:05:40 +0300

    Fix the zodiac sign that starts in december and ends in january next year.

--- ZodiacSignsService/ZodiacSignsService/Helper.cs
+++ ZodiacSignsService/ZodiacSignsService/Helper.cs
@@ -17,6 +17,13 @@ namespace ZodiacSignsService
             {
                 DateTime start = s.Interval.GetStartTime(date.Year.ToString());
                 DateTime end = s.Interval.GetEndTime(date.Year.ToString());
+                if(start.Month == 12 && end.Month == 1)
+                {
+                    start = (DateTime.ParseExact($"{start.Month}/{start.Day}/{start.Year - 1}", "M/d/yyyy", provider));
+                }
+                // 01/01/2020
+                // start = 12/21/2019
+                // end   = 1/19/2020
                 if (date >= start && date <= end)
                 {
                     return s.Name;
